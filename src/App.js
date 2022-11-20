a7cf580b577456ddb05169ee76fa2f1eccdf5ab7
Manish <manmanisharma@gmail.com>
2022-11-20 18:16:41 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 
-function App() {
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+    monsters:[ ],
+    searchField:''
+
+   }
+
+  }
+ 
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((res)=>res.json())
+    .then((users)=>this.setState(()=>{
+      return {monsters:users}
+    }));
+  
+}
+    
+  onSearchChange = (event)=>{
+    const searchField=event.target.value.toLowerCase()
+   
+     this.setState(()=>{
+       return   { searchField}
+     })
+   }
+  
+  render() {
+
+    const filteredMonsters=this.state.monsters.filter((monster)=>{
+          
+      return monster.name.toLowerCase().includes(this.state.searchField)
+    })
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='app-title'>
+        <h1>Monster-Pokedesk</h1>
+      </div>
+      <SearchBox  
+      className="search-monster" 
+      placeholder="search-box"
+       onChangeHandler={ this.onSearchChange }
+       />
+       
+     <CardList monsters={filteredMonsters}/>
+
+      
     </div>
+    
   );
+}
 }
 
 export default App;
